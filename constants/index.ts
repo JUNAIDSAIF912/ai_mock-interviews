610538668ac4ec287252c6831c7a0cc5610d0855
unknown <JunaidSaif>
2025-09-19 00:13:34 +0530

    setup app, files and folder structure, routing and auth pages

---
+++ constants/index.ts
@@ -0,0 +1,21 @@
+// import {CreateAssistantDTO } from "@vapi-ai/web/dist/api";
+// import { z } from "zod";
+//
+//
+// export const mappings = {
+//     "react.js": "react",
+//     reactjs: "react",
+//     react: "react",
+//     "next.js": "nextjs",
+//     nextjs: "nextjs",
+//     next: "nextjs",
+//     "vue.js": "vuejs",
+//     vuejs: "vuejs",
+//     vue:"vuejs"
+//     "express.js": "express",
+//     expressjs: "express",
+//     express:"express",
+//     "node.js": "nodejs",
+//     nodejs: "nodejs",
+//     node: "nodejs",
+// }
