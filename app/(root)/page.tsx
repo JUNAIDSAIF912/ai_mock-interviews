67b04706cd2e1a418d44cf286af2814632bb7e58
Junaid Saifi <junaidsaifi2127@gmail.com>
2025-09-29 23:37:50 +0530

    new components added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,7 +7,43 @@ import InterviewCard from "@/components/InterviewCard";
 
 const Page = () => {
     return (
-        <div>Home Page</div>
+        <>
+            <section className="card-cta">
+                <div className="flex flex-col gap-6 max-w-lg">
+                <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
+                <p className="text-lg">
+                    Practice on real interview questions % get instant feedback
+                </p>
+                    <Button asChild className="btn-primary max-sm:w-full">
+                        <Link href="/interview">Start an Interview</Link>
+
+                    </Button>
+                </div>
+                <Image src="/robot.png" alt="robo-dude" width={400} height={400} className="max-sm:hidden" />
+            </section>
+
+            <section className="flex flex-col gap-6 mt-8">
+                <h2>Your Interviews</h2>
+                <div className="interviews-section">
+                    {dummyInterviews.map((interview) => (
+                        <InterviewCard {...interview} />
+                    ))}
+                </div>
+            </section>
+
+            <section className="flex flex-col gap-6 mt-8">
+                <h2>Take an Interview</h2>
+
+                <div className="interviews-section">
+                    {dummyInterviews.map((interview) => (
+                        <InterviewCard {...interview} />
+                    ))}
+
+                    {/*<p>You haven't taken any interview ye</p>*/}
+
+                </div>
+            </section>
+        </>
     )
 }
 export default Page
