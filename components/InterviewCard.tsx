4f9b32c59320a492d255242db005b7f6623f89d1
Junaid Saifi <junaidsaifi2127@gmail.com>
2025-09-29 23:33:44 +0530

    new interviewCard file added

---
+++ components/InterviewCard.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const InterviewCard = ({ interviewId, userId, role, type, techstack, createdAt }:InterviewCardProps) => {
+    return (
+        <div>InterviewCard</div>
+    )
+}
+export default InterviewCard
